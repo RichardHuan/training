16ead2173cb3562b7a503ee569d87414f6123ab0
shengyushen <shengyushen@aliyun.com>
2020-01-17 21:32:46 +0800

    adding plt

--- recommendation/pytorch/plt.sh
+++ recommendation/pytorch/plt.sh
@@ -1,2 +1,7 @@
-gnuplot -p -e 'plot "p1_v100" u 1:2 w linesp , "p2_v100" u 1:2 w linesp, "p3_k80" u 1:2 w linesp'
+grep "Epoch.*HR" log1_v100 |awk '{print substr($2,1,length($2)-1) " " substr($5,1,length($5)-1)}' > p1_v100
+grep "Epoch.*HR" log2_v100 |awk '{print substr($2,1,length($2)-1) " " substr($5,1,length($5)-1)}' > p2_v100
+grep "Epoch.*HR" log3_k80  |awk '{print substr($2,1,length($2)-1) " " substr($5,1,length($5)-1)}' > p3_k80
+grep "Epoch.*HR" log4_k80  |awk '{print substr($2,1,length($2)-1) " " substr($5,1,length($5)-1)}' > p4_k80
+
+gnuplot -p -e 'plot "p1_v100" u 1:2 w linesp , "p2_v100" u 1:2 w linesp, "p3_k80" u 1:2 w linesp,"p4_k80" u 1:2 w linesp'
 
