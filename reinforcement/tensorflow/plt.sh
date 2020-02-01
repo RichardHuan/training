a3ecd84c33298d944e8b6af6cbaf3a28b3f58f74
shengyushen <shengyushen@aliyun.com>
2020-02-01 08:34:13 -0500

    plt a6

--- reinforcement/tensorflow/plt.sh
+++ reinforcement/tensorflow/plt.sh
@@ -1,7 +1,8 @@
 grep ^accuracy l1 > a1
 grep ^accuracy l2 > a2
 grep ^accuracy l5 > a5
+grep ^accuracy l6 > a6
 
 
-gnuplot -p -e 'plot "a1" u 0:3 w linesp, "a2" u 0:3 w linesp,"a5" u 0:3 w linesp'
+gnuplot -p -e 'plot "a1" u 0:3 w linesp, "a2" u 0:3 w linesp,"a5" u 0:3 w linesp,"a6" u 0:3 w linesp'
 
