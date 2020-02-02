c69885b1d91c9080656eaf91da3892ea137cbd1d
shengyushen <shengyushen@aliyun.com>
2020-02-01 22:53:26 -0500

    adding mnist

--- reinforcement/tensorflow/plt.sh
+++ reinforcement/tensorflow/plt.sh
@@ -1,8 +1,9 @@
 #grep ^accuracy l1 > a1
 #grep ^accuracy l2 > a2
 grep ^accuracy l5 > a5
-grep ^accuracy l6 > a6
+#grep ^accuracy l6 > a6
 
 
-gnuplot -p -e 'plot "a1" u 0:3 w linesp, "a2" u 0:3 w linesp,"a5" u 0:3 w linesp,"a6" u 0:3 w linesp'
+#gnuplot -p -e 'plot "a1" u 0:3 w linesp, "a2" u 0:3 w linesp,"a5" u 0:3 w linesp,"a6" u 0:3 w linesp'
+gnuplot -p -e 'f2(x)=b2*x+c2;fit f2(x) "a2" u 0:3 via b2,c2 ; plot "a2" u 0:3 w linesp,f2(x),"a5" u 0:3 w linesp'
 
