741e6fa488902562c00e9cfcbaad053ff330a30d
shengyushen <shengyushen@aliyun.com>
2020-02-10 02:27:29 -0500

    deleting old object det

--- object_detection/download_dataset.sh
+++
@@ -1,4 +0,0 @@
-wget https://dl.fbaipublicfiles.com/detectron/coco/coco_annotations_minival.tgz --no-check-certificate
-wget http://images.cocodataset.org/zips/train2014.zip --no-check-certificate
-wget http://images.cocodataset.org/zips/val2014.zip --no-check-certificate
-wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip --no-check-certificate
--- object_detection/proxy_host.sh
+++
@@ -1,3 +0,0 @@
-export http_proxy=http://127.0.0.1:3128
-export https_proxy=https://127.0.0.1:3128
-
