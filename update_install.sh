d258b2dc751c5cd950df62835546f5bb384e3350
shengyushen <shengyushen@aliyun.com>
2020-02-10 03:58:22 -0500

    new script to generate general docker

--- update_install.sh
+++ update_install.sh
@@ -1,5 +1,7 @@
 apt-get update -o Acquire::https::developer.download.nvidia.com::Verify-Peer=false
-apt install -y vim  python-setuptools  python-pip python3-pip virtualenv htop 
+apt install -y vim  python-setuptools  python-pip python3-pip virtualenv htop  libcupti-dev
+ln -s /usr/lib/x86_64-linux-gnu/libcupti.so.7.5.18 /usr/lib/x86_64-linux-gnu/libcupti.so.10.0
+
 pip3 install --upgrade pip
 pip3 install --upgrade setuptools
 pip3 install numpy scipy sklearn tf-nightly-gpu
