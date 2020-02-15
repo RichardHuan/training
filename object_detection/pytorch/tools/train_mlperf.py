1902245e16aefdede52c40224daa7824437f1edc
shengyushen <shengyushen@aliyun.com>
2020-02-14 22:15:03 -0500

    I reduce the eval period to 100 and run a whole night, I find mAP actually became non zero, so it is ok to continue

--- object_detection/pytorch/tools/train_mlperf.py
+++ object_detection/pytorch/tools/train_mlperf.py
@@ -36,6 +36,7 @@ from mlperf_compliance import mlperf_log
 
 def test_and_exchange_map(tester, model, distributed):
     results = tester(model=model, distributed=distributed)
+    #print("SSY results "+str(results))
 
     # main process only
     if is_main_process():
@@ -44,9 +45,11 @@ def test_and_exchange_map(tester, model, distributed):
         map_results, raw_results = results[0]
         bbox_map = map_results.results["bbox"]['AP']
         segm_map = map_results.results["segm"]['AP']
+        print("ssy11")
     else:
         bbox_map = 0.
         segm_map = 0.
+        print("ssy12")
 
     if distributed:
         map_tensor = torch.tensor([bbox_map, segm_map], dtype=torch.float32, device=torch.device("cuda"))
@@ -60,20 +63,31 @@ def mlperf_test_early_exit(iteration, iters_per_epoch, tester, model, distribute
     # Note: let iters / epoch == 10k, at iter 9999 we've finished epoch 0 and need to test
     if iteration > 0 and (iteration + 1)% iters_per_epoch == 0:
         epoch = iteration // iters_per_epoch
+        print("ssy1")
 
         print_mlperf(key=mlperf_log.EVAL_START, value=epoch)
+        print("ssy2")
+        #print("tester "+str(tester))
+        #print("model "+str(model))
 
         bbox_map, segm_map = test_and_exchange_map(tester, model, distributed)
+        print("ssy3")
         # necessary for correctness
         model.train()
+        print("ssy4")
 
         print_mlperf(key=mlperf_log.EVAL_TARGET, value={"BBOX": min_bbox_map,
                                                         "SEGM": min_segm_map})
+        print("ssy5")
         logger = logging.getLogger('maskrcnn_benchmark.trainer')
+        print("ssy6")
         logger.info('bbox mAP: {}, segm mAP: {}'.format(bbox_map, segm_map))
+        print("ssy7")
 
         print_mlperf(key=mlperf_log.EVAL_ACCURACY, value={"epoch" : epoch, "value":{"BBOX" : bbox_map, "SEGM" : segm_map}})
+        print("ssy8")
         print_mlperf(key=mlperf_log.EVAL_STOP)
+        print("ssy9")
 
         # terminating condition
         if bbox_map >= min_bbox_map and segm_map >= min_segm_map:
@@ -126,7 +140,8 @@ def train(cfg, local_rank, distributed):
     print_mlperf(key=mlperf_log.ASPECT_RATIOS, value=cfg.MODEL.RPN.ASPECT_RATIOS)
     print_mlperf(key=mlperf_log.BACKBONE, value=cfg.MODEL.BACKBONE.CONV_BODY)
     print_mlperf(key=mlperf_log.NMS_THRESHOLD, value=cfg.MODEL.RPN.NMS_THRESH)
-
+    # /root/ssy/maskrcnn-benchmark/maskrcnn_benchmark/modeling/detector/detectors.py
+    # building bare mode without doing anthing
     model = build_detection_model(cfg)
     device = torch.device(cfg.MODEL.DEVICE)
     model.to(device)
@@ -170,6 +185,9 @@ def train(cfg, local_rank, distributed):
         is_distributed=distributed,
         start_iter=arguments["iteration"]
     )
+    print("SSY iters_per_epoch "+str(iters_per_epoch))
+    #print("SSY iters_per_epoch change to 100 ")
+    #iters_per_epoch = 100
 
     checkpoint_period = cfg.SOLVER.CHECKPOINT_PERIOD
 
@@ -193,6 +211,7 @@ def train(cfg, local_rank, distributed):
     per_iter_callback_fn = functools.partial(
             mlperf_test_early_exit,
             iters_per_epoch=iters_per_epoch,
+            # /root/ssy/maskrcnn-benchmark/maskrcnn_benchmark/engine/tester.py
             tester=functools.partial(test, cfg=cfg),
             model=model,
             distributed=distributed,
@@ -200,7 +219,7 @@ def train(cfg, local_rank, distributed):
             min_segm_map=0.339)
 
     start_train_time = time.time()
-
+    # /root/ssy/maskrcnn-benchmark/maskrcnn_benchmark/engine/trainer.py
     do_train(
         model,
         data_loader,
