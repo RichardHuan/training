e19a95e9d761da3e8a3de3b1332fa16b27c6292b
shengyushen <shengyushen@aliyun.com>
2020-02-18 09:03:19 -0500

    adding new ssd bf16 with built in bf16cut

---
+++ rnn_translator_3DBF16/pytorch/seq2seq/models/pgrad.py
@@ -0,0 +1,41 @@
+# SSY
+import torch.autograd
+from torch.autograd import Function
+from torch.nn import Module 
+# SSY avoid lots of intermedia result space
+def float2bf16(xmlp):
+        aabs=torch.abs(xmlp)
+        rnd2exp=torch.pow(2,torch.floor(torch.where(aabs==0.0,aabs,torch.log2(aabs)))) # get back the old value rounded to 2 exp
+        return  torch.mul(torch.mul(torch.div(torch.floor(torch.mul(torch.div(aabs,rnd2exp),256)),256),rnd2exp),torch.sign(xmlp))
+
+class bf16cutfp(Function):
+    @staticmethod
+    def forward(ctx, tensor):
+        return float2bf16(tensor)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output
+
+class bf16cutbp(Function):
+    @staticmethod
+    def forward(ctx, tensor):
+        return tensor
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return float2bf16(grad_output)
+
+class bf16cutfp_mod(Module):
+  def __init__(self):
+    super(bf16cutfp_mod,self).__init__()
+
+  def forward(self,input):
+    return bf16cutfp.apply(input)
+
+class bf16cutbp_mod(Module):
+  def __init__(self):
+    super(bf16cutbp_mod,self).__init__()
+
+  def forward(self,input):
+    return bf16cutbp.apply(input)
