0b3c10ba7129dfb0f2db72cbd339ef58745b03ea
shengyushen <shengyushen@aliyun.com>
2020-02-23 22:29:52 -0500

    adding maskrcnn modified code

---
+++ image_classification_3dir_bf16/tensorflow/modified_system_code/cp.sh
@@ -0,0 +1,5 @@
+cp core.py /usr/local/lib/python3.5/dist-packages/tensorflow_core/python/keras/layers/core.py
+cp convolutional.py /usr/local/lib/python3.5/dist-packages/tensorflow_core/python/keras/layers/convolutional.py
+cp /root/ssy/tensorflow_1.15.2/tensorflow/tensorflow/examples/adding_an_op/bf16*.so /usr/local/lib/python3.5/dist-packages/tensorflow_core/python/keras/layers/
+cp /root/ssy/tensorflow_1.15.2/tensorflow/tensorflow/examples/adding_an_op/bf16*.py /usr/local/lib/python3.5/dist-packages/tensorflow_core/python/keras/layers/
+cp ./bf16*.py /usr/local/lib/python3.5/dist-packages/tensorflow_core/python/keras/layers/
