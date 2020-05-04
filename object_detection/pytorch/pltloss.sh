6cd8fcf0d80472f4fff4ec7e478d6b5f4c8cbfbd
shengyushen <shengyushen@aliyun.com>
2020-05-04 03:37:05 -0400

    resnet

--- object_detection/pytorch/pltloss.sh
+++ object_detection/pytorch/pltloss.sh
@@ -10,19 +10,6 @@ grep maskrcnn_benchmark.trainer ../../object_detection_BF16/pytorch/bf16_1.log  
 grep maskrcnn_benchmark.trainer bf16_3dir_1.log                                             |awk '{if($5=="eta:") { if($7=="iter:") {save=$8} else {save=$10} } else if ($6=="mAP:") {print save " " $7 " " $10} }' > bf16_3dir_1.map
 grep maskrcnn_benchmark.trainer ../../object_detection_3dir_bf12/pytorch/bf10_3dir_1.log    |awk '{if($5=="eta:") { if($7=="iter:") {save=$8} else {save=$10} } else if ($6=="mAP:") {print save " " $7 " " $10} }' > bf10_3dir_1.map
 
-gnuplot -p -e '
-  set key left top;
-  set xlabel "Iteration";
-  set ylabel "loss";
-  set y2label "mAP(bbox/segm)";
-  set ytics nomirror; 
-  set y2tics 0.1,0.01;
-  plot 
-    0.339                             axis x1y2 title "SEGM mAP target", 
-    "fp32_1.map"       u 1:3 w linesp axis x1y2 title "FP32 SEGM mAP"      lc 1 pt 1, 
-    "bf16_3dir_1.map"  u 1:3 w linesp axis x1y2 title "BF12 3dir SEGM mAP" lc 3 pt 3, 
-    "bf10_3dir_1.map"  u 1:3 w linesp axis x1y2 title "BF10 3dir SEGM mAP" lc 4 pt 4
-'
 #gnuplot -p -e '
 #  set key left top;
 #  set xlabel "Iteration";
@@ -33,14 +20,27 @@ gnuplot -p -e '
 #  plot 
 #    0.339                             axis x1y2 title "SEGM mAP target", 
 #    "fp32_1.map"       u 1:3 w linesp axis x1y2 title "FP32 SEGM mAP"      lc 1 pt 1, 
-#    "fp32_1.loss"      u 1:2          axis x1y1 title "FP32 loss"          lc 1 pt 1, 
-#    "bf16_1.map"       u 1:3 w linesp axis x1y2 title "BF16 SEGM mAP"      lc 2 pt 2, 
-#    "bf16_1.loss"      u 1:2          axis x1y1 title "BF16 loss"          lc 2 pt 2,
 #    "bf16_3dir_1.map"  u 1:3 w linesp axis x1y2 title "BF12 3dir SEGM mAP" lc 3 pt 3, 
-#    "bf16_3dir_1.loss" u 1:2          axis x1y1 title "BF12 3dir loss"     lc 3 pt 3,
-#    "bf10_3dir_1.map"  u 1:3 w linesp axis x1y2 title "BF10 3dir SEGM mAP" lc 4 pt 4, 
-#    "bf10_3dir_1.loss" u 1:2          axis x1y1 title "BF10 3dir loss"     lc 4 pt 4
+#    "bf10_3dir_1.map"  u 1:3 w linesp axis x1y2 title "BF10 3dir SEGM mAP" lc 4 pt 4
 #'
+gnuplot -p -e '
+  set key left top;
+  set xlabel "Iteration";
+  set ylabel "loss";
+  set y2label "mAP(bbox/segm)";
+  set ytics nomirror; 
+  set y2tics 0.1,0.01;
+  plot 
+    0.339                             axis x1y2 title "SEGM mAP target", 
+    "fp32_1.map"       u 1:3 w linesp axis x1y2 title "FP32 SEGM mAP"      lc 1 pt 1, 
+    "fp32_1.loss"      u 1:2          axis x1y1 title "FP32 loss"          lc 1 pt 1, 
+    "bf16_1.map"       u 1:3 w linesp axis x1y2 title "BF16 SEGM mAP"      lc 2 pt 2, 
+    "bf16_1.loss"      u 1:2          axis x1y1 title "BF16 loss"          lc 2 pt 2,
+    "bf16_3dir_1.map"  u 1:3 w linesp axis x1y2 title "BF12 3dir SEGM mAP" lc 3 pt 3, 
+    "bf16_3dir_1.loss" u 1:2          axis x1y1 title "BF12 3dir loss"     lc 3 pt 3,
+    "bf10_3dir_1.map"  u 1:3 w linesp axis x1y2 title "BF10 3dir SEGM mAP" lc 4 pt 4, 
+    "bf10_3dir_1.loss" u 1:2          axis x1y1 title "BF10 3dir loss"     lc 4 pt 4
+'
 # old version with bbox accuracy
 #gnuplot -p -e '
 #  set key left top;
